0df23e246db10a5a8381107bdfcefbd076209173
raqcames <raqueel.camargo@gmail.com>
2021-01-18 20:03:23 -0300

    Atividade realizada

--- main.ts
+++ main.ts
@@ -2,6 +2,11 @@ function hello(q: string): void {
   console.log('Hello', q)
 }
 
+function bye(q: string): void {
+  console.log('Bye', q)
+}
+
 const quem: string = 'World'
 
 hello(quem)
+bye(quem)
